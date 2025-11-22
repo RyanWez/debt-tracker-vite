f0b705d39844b890dd863a47eeaf6b4c2661b305
Ryan Wez <ryanwez25@gmail.com>
2025-11-22 10:48:43 +0630

    fix header spacer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,6 +8,11 @@ export default function Layout() {
         <div className="flex min-h-screen bg-gray-50 font-sans">
             <Sidebar />
             <div className="flex-1 flex flex-col min-w-0 pb-16 md:pb-0">
+                {/* Desktop Header Spacer to align with Sidebar */}
+                <div className="hidden md:block p-4 border-b-3 border-black bg-white sticky top-0 z-10">
+                    <div className="h-8"></div> {/* Matches logo height */}
+                </div>
+
                 <Header />
                 <main className="flex-1 p-4 md:p-8 max-w-7xl mx-auto w-full">
                     <Outlet />
